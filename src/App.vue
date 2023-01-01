36d054ed63ff920f070099aae80470e13e37fec6
이윤기 <ahacross@naver.com>
2023-01-01 23:23:10 +0900

    json으로 data 분리
    연습 목록 추가 되면 json 파일만 수정하면 됨

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
     <q-expansion-item
       v-for="({ label, list }, idx) in practiceList"
       :key="idx"
+      group="practiceGroup"
       :label="label">
       <q-card>
         <q-card-section>
           <template v-for="(item, monthIdx) in list" :key="monthIdx">
-            <span>
+            <div :class="{ highLight: highlightsDate === item.date }">
               <span style="margin-right: 5px">
-                {{ item.date }} - {{ item.title }} {{ item.description }}
+                {{ item.date.split('.')[2] + '일' }} - {{ item.title }}
+                {{ item.description }}
               </span>
               <br />
               <a
@@ -20,6 +22,30 @@
                     ? item.url[partIdx]
                     : item.url.split('mp').join(part.short)
                 "
+                target="_blank">
+                {{ part.text }}
+              </a>
+            </div>
+            <br />
+            <br />
+          </template>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <q-expansion-item group="practiceGroup" label="송영">
+      <q-card>
+        <q-card-section>
+          <template v-for="(item, idx) in 송영" :key="idx">
+            <span>
+              <span style="margin-right: 5px">
+                {{ item.description }} {{ item.title }}
+              </span>
+              <br />
+              <a
+                v-for="(part, partIdx) in partList.slice(1)"
+                :key="partIdx"
+                :href="item.url[partIdx]"
                 target="_blank">
                 {{ part.text }}
               </a>
@@ -34,6 +60,7 @@
 </template>
 
 <script>
+import dayjs from 'dayjs'
 export default {
   data() {
     return {
@@ -45,50 +72,28 @@ export default {
         { text: '테너', short: 'tt' },
         { text: '베이스', short: 'bb' }
       ],
-      practiceList: [
-        {
-          label: '1월 성가곡 연습',
-          list: [
-            {
-              date: '1일',
-              title: '주님께 감사',
-              description: '중앙성가 19 (p.74)',
-              url: 'http://ad2007.iptime.org/Web/jaart/cor/19/1-1/u18_data10_mp.html'
-            },
-            {
-              date: '8일',
-              title: '오직 주만 따르오리',
-              description: '중앙성가 19 (p.95)',
-              url: 'http://ad2007.iptime.org/Web/jaart/cor/19/1-1/u18_data12_mp.html'
-            },
-            {
-              date: '15일',
-              title: '심령이 가난한 자는',
-              description: '중앙성가 19 (p.142)',
-              url: 'http://ad2007.iptime.org/Web/jaart/cor/19/1-1/u18_data17_mp.html'
-            },
-            {
-              date: '22일',
-              title: '내 진정 사모하는',
-              description: '새찬송가 (88장)',
-              type: 'youtube',
-              url: [
-                'https://m.youtube.com/watch?v=LwSFBLbnfhI',
-                'https://m.youtube.com/watch?v=zYVFKnEPdUs',
-                'https://m.youtube.com/watch?v=5t-1CNsnI0Q',
-                'https://m.youtube.com/watch?v=lGrAt6hlTUQ',
-                'https://m.youtube.com/watch?v=BT-skgAJX_o'
-              ]
-            },
-            {
-              date: '29일',
-              title: '십자가를 질 수 있나',
-              description: '중앙성가 19 (p.156)',
-              url: 'http://ad2007.iptime.org/Web/jaart/cor/19/1-1/u18_data19_mp.html'
-            }
-          ]
-        }
-      ]
+      practiceList: [],
+      송영: [],
+      highlightsDate: ''
+    }
+  },
+  async mounted() {
+    this.setHighLightDate()
+
+    const res = await fetch('/songList.json')
+    this.practiceList = await res.json()
+
+    const res2 = await fetch('/송영.json')
+    this.송영 = await res2.json()
+  },
+  methods: {
+    setHighLightDate() {
+      const today = dayjs()
+      let date = null
+      if (today.day()) {
+        date = today.add(7 - today.day(), 'd')
+      }
+      this.highlightsDate = (date || today).format('YYYY.MM.DD')
     }
   }
 }
@@ -133,5 +138,9 @@ a:visited {
 }
 a:hover,
 a:active {
+}
+
+.highLight {
+  background: coral;
 }
 </style>
